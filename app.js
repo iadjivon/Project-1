03fe859d44ce39f68d07ace4e39009166d992a4f
Ida Adjivon <65119712+iadjivon@users.noreply.github.com>
2020-08-26 01:14:51 -0400

    city data now loading. woohoo.

--- app.js
+++ app.js
@@ -10,26 +10,20 @@
 
 $('.Submit').on('click', (event) => {
     event.preventDefault();
-
     console.log( $('.input-bar').val() );
-
-
-  const locations = $('input').val();
+    const locations = $('.input-bar').val();
 
 
 $.ajax({
     url: `https://api.ipgeolocation.io/astronomy?apiKey=b391913d2c124b18981c98030c6a73c2&location=${locations}`,
-    // url:`https://api.ipgeolocation.io/astronomy?apiKey=b391913d2c124b18981c98030c6a73c2&location=${locations}`, 
-    success: (data)=>{
+}).then((data)=>{
         console.log(data);
-        $('#city').html(data.location.city);
+        $('#city').html(data.location.location);
         $('#sunrise').html(data.sunrise);
         $('#sunset').html(data.sunset);
     },
-    error: ()=>{
+    ()=>{
         console.log('bad request');
-    }
-
 });
 });
 
