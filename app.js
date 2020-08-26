d19eced2cc7b72d4d89b4f3c681fa25eb7ed3d5e
Ida Adjivon <65119712+iadjivon@users.noreply.github.com>
2020-08-26 00:39:37 -0400

    api added, sunrise and sunset data load, city data not loading well

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-////////////////////////
+///////////////////////
 //HEADER
 ////////////////////////
-console.log("javascrip is all set")
 
 
 
@@ -9,11 +8,30 @@ console.log("javascrip is all set")
 //FIRST SECTION 
 ////////////////////////
 
-$('.Submit').on('click', () => {
-    console.log( $('.input-bar').val() );
-  });
+$('.Submit').on('click', (event) => {
+    event.preventDefault();
 
-  
+    console.log( $('.input-bar').val() );
+
+
+  const locations = $('input').val();
+
+
+$.ajax({
+    url: `https://api.ipgeolocation.io/astronomy?apiKey=b391913d2c124b18981c98030c6a73c2&location=${locations}`,
+    // url:`https://api.ipgeolocation.io/astronomy?apiKey=b391913d2c124b18981c98030c6a73c2&location=${locations}`, 
+    success: (data)=>{
+        console.log(data);
+        $('#city').html(data.location.city);
+        $('#sunrise').html(data.sunrise);
+        $('#sunset').html(data.sunset);
+    },
+    error: ()=>{
+        console.log('bad request');
+    }
+
+});
+});
 
 
 
