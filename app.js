641704f8260daae1d6eb2b771299bbb828b8cb89
Ida Adjivon <65119712+iadjivon@users.noreply.github.com>
2020-08-26 23:56:53 -0400

    new background image added, search bar design complete, calendar available but need readjustment on page

--- app.js
+++ app.js
@@ -10,14 +10,19 @@
 
 $('.Submit').on('click', (event) => {
     event.preventDefault();
-    console.log( $('.input-bar').val() );
+    // console.log( $('.input-bar').val() );
     const locations = $('.input-bar').val();
-
 
 $.ajax({
     url: `https://api.ipgeolocation.io/astronomy?apiKey=b391913d2c124b18981c98030c6a73c2&location=${locations}`,
 }).then((data)=>{
-        console.log(data);
+        // console.log(data);
+        // console.log($("#specific-date")).value;
+        // console.log($("#specific-date")).clientLeft;
+
+
+        //  console.log($('input[type="date"]'))[0];
+        // console.log(date);
         $('#city').html(data.location.location);
         $('#sunrise').html(data.sunrise);
         $('#sunset').html(data.sunset);
